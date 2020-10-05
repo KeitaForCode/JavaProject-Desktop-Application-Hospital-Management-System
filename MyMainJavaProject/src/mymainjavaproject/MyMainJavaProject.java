5056550ee44b9c2393f289ce87b93173effd4eba
KeitaForCode <keita.losene@yahoo.com>
2020-10-05 14:24:00 +0100

    'java_project'

---
+++ MyMainJavaProject/src/mymainjavaproject/MyMainJavaProject.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mymainjavaproject;
+
+/**
+ *
+ * @author Leo
+ */
+public class MyMainJavaProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DashBoard leo = new DashBoard();
+        // TODO code application logic here
+    }
+    
+}
